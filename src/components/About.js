121772f90ef0039e7e52fc57b6076e8532cc72a9
Kaity <kaitlynn281@gmail.com>
2019-06-08 13:56:03 -0400

    routing

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class About extends Component {
+    render() {
+        return <div>{this.props.isCustom}</div>;
+    }
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
 import Items from './components/Items';
 import Hero from './components/Hero';
+import Home from './components/Home';
 import './css/shop.css';
 
 const dataUrl = './real-user-data.json';
@@ -9,6 +13,22 @@ const dataUrl = './real-user-data.json';
 render(
     <React.Fragment>
         <Hero />
+        <Home />
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" render={() => <About isCustom="hello" />} />
+            <Route path="/contact" component={Contact} />
+                <Route component={NotFound} />
+        </Switch>
         <Items data-url={dataUrl} />
     </React.Fragment>,
     document.getElementById('app')
